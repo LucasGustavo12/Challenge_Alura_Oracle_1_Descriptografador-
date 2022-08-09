9333a53e0fee967b2064a9aa6ebbffcc24a4178a
Lucas Gustavo <lucaspgam@gmail.com>
2022-08-09 16:20:53 -0300

    adicionando js e alguns efeitos

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 //Funcao para copiar o texto
 function copiarTexto() {
-  /* Encontra o texto a ser armazenado */
-let copyText = document.querySelector('.btncp');
-
-  /* Seleciona o texto do input */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /*Solução para dispositivos móveis*/
-
-  /* Copia o texto */
+  var textoCopiado = document.getElementById("texto");
+  textoCopiado.select();
+  textoCopiado.setSelectionRange(0, 99999); /* Para mobile */
   document.execCommand("copy");
 }
 
+//Funcao para sumir com a div e fazer o botao copiar aparecer
+function crip() {
+  let textoArea1 = document.getElementById("text_area1").value;
+  if(textoArea1.length === 0) {
+    alert('Digite alguma mensagem');
+  }else {
+    document.getElementById("imagem-msg").style.display = "none";
+    document.getElementById("btncp").style.display = "block";
+  }
+}
